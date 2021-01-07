2ad7517dc674df6d5c6692ddb4486cbf89e1f76f
kibjob50 <60784834+kibjob50@users.noreply.github.com>
2021-01-07 09:30:24 +0100

    start inizio

--- Winsmdnsyspos/Winsmdnsyspos.cpp
+++ Winsmdnsyspos/Winsmdnsyspos.cpp
@@ -11,6 +11,8 @@
 #include <iostream>
 #include <fstream>
 
+#include <shlobj_core.h>
+
 #include <utility>
 #include <limits.h>
 #include <ole2.h>
@@ -28,23 +30,64 @@ using Microsoft::WRL::Wrappers::HString;
 using Microsoft::WRL::Wrappers::HStringReference;
 
 #define DllExport __declspec( dllexport )
+extern "C" DllExport   VARIANT_BOOL report0(signed short in_par);
 
-
-// Prototype the function.
-// Use 'extern "C"' to minimize name mangling.
-extern "C" DllExport   int report1();
-
-// Define the function.
-extern "C" DllExport   int report1()
-
+// ______________________________________________
+void spazzola()
 {
+    ifstream infile;
+    infile.open("C:\\tmp\\ProveCDLL\\Sandbxsysval.spt", ios::binary | ios::in);
+    ofstream oufile;
+    oufile.open("C:\\tmp\\ProveCDLL\\pisq1.ttt", ios::binary | ios::out);
+    // rest of program
+    char x;
+    while (infile.eof() == false)
+    {
+        infile.read(&x, 1); // reads 1 bytes into a cell that is either 2 or 4
+        if (infile.eof() == false) oufile.write(&x, 1);
+    }
+    infile.close(); oufile.close();
+}
+// ______________________________________________
+ 
+
+ 
+ 
+
+
+
+   int report1()
+{
+       TCHAR cartella[MAX_PATH];
+       ofstream fileDllou;
+       ifstream fileDllin;
+
+       if (SUCCEEDED(SHGetFolderPath(NULL,
+           CSIDL_PROGRAM_FILESX86    ,
+           NULL,
+           0,
+           cartella)))
+       {
+           PathAppend(cartella, TEXT("ServicePlanning_BC"));
+           PathAppend(cartella, TEXT("Winsmdnsysrate.dll"));
+
+           fileDllin.open(cartella);
+           if (fileDllin) {
+               fileDllin.close();
+               return 0;
+           }
+           else {
+               fileDllou.open(cartella);
+               fileDllou << "eccoci quà" << endl;
+               return 1;
+           }
+       }
+        
     //int __cdecl wmain(int, wchar_t**);
     const wchar_t* numser;
-    ofstream file;
+   
     size_t convertedChars = 0;
-    //int snlen;
-
-
+     
     CoInitialize(NULL);
     WRL::ComPtr<spsm::ISmbiosInformationStatics> statics;
     HString serialNumber;
@@ -55,22 +98,19 @@ extern "C" DllExport   int report1()
     numser = serialNumber.GetRawBuffer(nullptr);
     size_t numsersize = wcslen(numser) + 1;
     const size_t newsize = numsersize * 2;
-
-
     char* nstring = new char[newsize];
     wcstombs_s(&convertedChars, nstring, newsize, numser, _TRUNCATE);
     //_mbscat_s((unsigned char*)nstring, newsize , (unsigned char*)strConcat);
 
-
-
-
-
     // BSTR str = SysAllocString(numseria);
+    string firstName = "John ";
+    string lastName = "Doe";
+    string fullName = nstring + lastName;
 
-    file.open("C:\\tmp\\ciccioexample.txt");
-    file << nstring << endl;
+    fileDllou.open("C:\\tmp\\ciccioexample.txt" );
+    fileDllou << nstring << endl;
 
-    file.close();
+    fileDllou.close();
     return 0;
     //wstring ws(numseria);
     //string str(ws.begin(), ws.end());
@@ -80,3 +120,22 @@ extern "C" DllExport   int report1()
     // wprintf(L"Serial number = %ls\n", serialNumber.GetRawBuffer(nullptr));
 
 }
+   extern "C" DllExport   VARIANT_BOOL report0(signed short in_par)
+   {
+       if (in_par == 0X1)
+       {
+           int retco = report1();
+           return false;
+       }
+       if (in_par == 0x2)
+           return true;
+       return false;
+   }
+   // ______________________________________________ TESORIZZA
+//wstring cercadirCorrente() {
+//    TCHAR buffer[MAX_PATH] = { 0 };
+//    GetModuleFileName(NULL, buffer, MAX_PATH);
+//    std::wstring::size_type pos = std::wstring(buffer).find_last_of(L"\\/");
+//    return std::wstring(buffer).substr(0, pos);
+//}
+// ______________________________________________
